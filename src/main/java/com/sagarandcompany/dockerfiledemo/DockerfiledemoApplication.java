2f82c4937d15d275492ebccf1c4d1bc149075237
sagarmal624 <sagarmal624@gmail.com>
2018-12-31 19:40:37 +0530

    change docker war file name

--- src/main/java/com/sagarandcompany/dockerfiledemo/DockerfiledemoApplication.java
+++ src/main/java/com/sagarandcompany/dockerfiledemo/DockerfiledemoApplication.java
@@ -2,6 +2,7 @@ package com.sagarandcompany.dockerfiledemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -10,6 +11,10 @@ public class DockerfiledemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DockerfiledemoApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder
+														 application) {
+		return application.sources(DockerfiledemoApplication.class);
+	}
 }
 
